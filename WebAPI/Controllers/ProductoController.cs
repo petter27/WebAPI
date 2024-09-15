ebbf2a79be29182b4a3a998241846087bc3519cd
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-14 19:27:41 -0600

    Creando controlador para metodos de productos

---
+++ WebAPI/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Custom;
+using WebAPI.Models;
+using WebAPI.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)] // solo usuarios autorizados, si hay multiples formas de autorizacion
+    [Authorize]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+
+        private readonly DbpruebaContext _dbpruebaContext;
+        public ProductoController(DbpruebaContext dbpruebaContext)
+        {
+            _dbpruebaContext = dbpruebaContext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var lista = await _dbpruebaContext.Productos.ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, new { value = lista });
+        }
+    }
+}
